6705e60f21d557f2422bb9f272ec00c6ae6f41be
Orochi <orochi705@gmail.ru>
2022-05-03 14:26:43 +0900

    push

---
+++ Task_2.2/Program.cs
@@ -0,0 +1,5 @@
+﻿int x = new Random().Next(100, 999);
+System.Console.WriteLine(x);
+int a = int.Parse(Convert.ToString(x)[0].ToString());
+int b = int.Parse(Convert.ToString(x)[2].ToString());
+System.Console.WriteLine($"{a}{b}");

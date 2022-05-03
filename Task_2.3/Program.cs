dcdf3dd65ca55bde5e03fbfa7f1bfe9b5f877835
Orochi <orochi705@gmail.ru>
2022-05-03 14:28:14 +0900

    push

---
+++ Task_2.3/Program.cs
@@ -0,0 +1,5 @@
+﻿ int x = 32679;
+if (x > 100) 
+    {int a = int.Parse(Convert.ToString(x)[2].ToString());
+    Console.WriteLine("третья цифра " + a);}
+    else Console.WriteLine("третьей цифры нет ");
